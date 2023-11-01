2853350a2e9bc0277ec4758a8597048232e1c4ea
dedrickj1 <dedrickjackson11@gmail.com>
2023-11-01 09:48:23 -0500

    Update to Homework

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb013ce0c676dfde280ba245'
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title= 'Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title= 'Login', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
