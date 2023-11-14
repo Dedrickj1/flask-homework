65c8a21c1353ba632aa6f7977884750d6c879ed4
dedrickj1 <dedrickjackson11@gmail.com>
2023-11-14 17:10:58 -0600

    update2

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy 
+from flask_login import LoginManager, UserMixin, login_user, login_required,logout_user, current_user
 from config import Config
-from forms import RegistrationForm, LoginForm
+# from forms import RegistrationForm, UserLoginForm
+from models import User, db, check_password_hash
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb013ce0c676dfde280ba245'
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True)
+    
+    
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(use_id))
 
 @app.route('/')
-@app.route("/home")
+def index():
+    return render_template('home.html', title='Home')
+
+
+@app.route("/profile")
+@login_required
 def home():
-    return render_template('home.html')
+    user = User.query.filter_by(username='Dedrick').first()
+    login_user(user)
+    return render_template()
 
 @app.route("/about")
 def about():
@@ -23,6 +46,15 @@ def register():
 def login():
     form = LoginForm()
     return render_template('login.html', title= 'Login', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return 'You are now logged out!'
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from forms import RegistrationForm, UserLoginForm
 
 
 class RegistrationForm(FlaskForm):
